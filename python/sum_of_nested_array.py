7977166b370f1848646b640988f0c308c060630a
Muhammad Haziq <beinghaziq@gmail.com>
2024-08-19 18:32:14 +0500

    exercise done for sum or nested array

---
+++ python/sum_of_nested_array.py
@@ -0,0 +1,12 @@
+def flatten(array):
+    flatten_array = []
+    for element in array:
+        if (isinstance(element, list)):
+            flatten_array.extend(flatten(element))
+        else:
+            if not isinstance(element, str):
+                flatten_array.append(element)
+    return flatten_array
+
+nested_list = [1, 2, [3, [4], 5, "6"], 6]
+print(sum(flatten(nested_list)))
